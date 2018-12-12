c6cf8c975b4a2ae7cfee4e873a8e8a17195ee197
lizardqueen98 <nadija.b@gmail.com>
2018-12-12 17:27:53 +0100

    pokusaj

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Observable;
+import java.util.ResourceBundle;
+
 public class Controller {
+    public TextField rijec;
+    public ListView lista;
+    public Button trazi;
+    private ListaFajlovaModel model;
+    /*public Controller(ListaFajlovaModel model){
+        this.model = model;
+    }*/
+    public void trazi(ActionEvent actionEvent) {
+        model = new ListaFajlovaModel();
+        model.napuni(rijec.getText(),"C:\\Users\\Nadija");
+        lista.setItems(model.getFajlovi());
+    }
+
 }
